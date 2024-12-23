efdc73b2e404444dce05c9e7d3cf94f90105a6f0
Oluwadunsin <oluwadunsin2016@gmail.com>
2024-12-23 14:50:24 +0100

    Modified

--- server.js
+++ server.js
@@ -37,16 +37,24 @@ res.json({
 const PORT = process.env.PORT || 4000;
 
 // Database connection and server start
+//     app.listen(PORT, () => {
+//     console.log('DB Config:', {
+//   username: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     host: process.env.DB_HOST,
+//     port:process.env.DB_PORT,
+//     dialect: "postgres"
+// });
+//       console.log(`Server is running on http://localhost:${PORT}`);
+//     });
+sequelize
+  .authenticate().then(() => {
+    console.log('Database connected successfully.');
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
-// sequelize
-//   .authenticate().then(() => {
-//     console.log('Database connected successfully.');
-//     app.listen(PORT, () => {
-//       console.log(`Server is running on http://localhost:${PORT}`);
-//     });
-//   })
-//   .catch(err => {
-//     console.error('Unable to connect to the database:', err.message);
-//   });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err.message);
+  });
