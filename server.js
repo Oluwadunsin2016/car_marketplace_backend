564b9e82d7479fce55e2dcdca6a489b0f9e8287b
Oluwadunsin <oluwadunsin2016@gmail.com>
2024-12-23 14:43:35 +0100

    Modified

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const userRoutes=require('./routes/userRoutes')
 const { sequelize } = require('./models');
 const cors = require('cors');
 
-const allowedOrigins = ['https://car-marketplace-frontend.vercel.app', 'http://localhost:5173'];
+const allowedOrigins = ['https://car-marketplace-frontend.vercel.app/', 'http://localhost:5173'];
 const app = express();
 app.use(express.json());
 // app.use(cors({ credentials:true })); // Replace with your React app’s origin
@@ -37,24 +37,16 @@ res.json({
 const PORT = process.env.PORT || 4000;
 
 // Database connection and server start
-//     app.listen(PORT, () => {
-//     console.log('DB Config:', {
-//   username: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_NAME,
-//     host: process.env.DB_HOST,
-//     port:process.env.DB_PORT,
-//     dialect: "postgres"
-// });
-//       console.log(`Server is running on http://localhost:${PORT}`);
-//     });
-sequelize
-  .authenticate().then(() => {
-    console.log('Database connected successfully.');
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
     });
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err.message);
-  });
+// sequelize
+//   .authenticate().then(() => {
+//     console.log('Database connected successfully.');
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err.message);
+//   });
